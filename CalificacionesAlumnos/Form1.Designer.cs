0320d4bc6dd2472e60f49be73575771583473de8
Ivan Tapia Avalos <ivan.tapia@mediaccess.com.mx>
2024-01-22 18:15:03 -0600

    Aplicacion de escritorio calificaciones alumnos

---
+++ CalificacionesAlumnos/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿
+namespace CalificacionesAlumnos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblCalificacion = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtCalificacion = new System.Windows.Forms.TextBox();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnBorrar = new System.Windows.Forms.Button();
+            this.btnMostrar = new System.Windows.Forms.Button();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.btnEliminarTodo = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.lblName.Location = new System.Drawing.Point(35, 53);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(76, 20);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Nombre:";
+            // 
+            // lblCalificacion
+            // 
+            this.lblCalificacion.AutoSize = true;
+            this.lblCalificacion.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.lblCalificacion.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCalificacion.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.lblCalificacion.Location = new System.Drawing.Point(383, 53);
+            this.lblCalificacion.Name = "lblCalificacion";
+            this.lblCalificacion.Size = new System.Drawing.Size(106, 20);
+            this.lblCalificacion.TabIndex = 1;
+            this.lblCalificacion.Text = "Calificacion:";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(182, 55);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(182, 20);
+            this.txtName.TabIndex = 2;
+            // 
+            // txtCalificacion
+            // 
+            this.txtCalificacion.Location = new System.Drawing.Point(495, 55);
+            this.txtCalificacion.Name = "txtCalificacion";
+            this.txtCalificacion.Size = new System.Drawing.Size(209, 20);
+            this.txtCalificacion.TabIndex = 3;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.BackColor = System.Drawing.Color.Lime;
+            this.btnGuardar.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuardar.Location = new System.Drawing.Point(392, 101);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(101, 39);
+            this.btnGuardar.TabIndex = 4;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = false;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // btnBorrar
+            // 
+            this.btnBorrar.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.btnBorrar.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBorrar.Location = new System.Drawing.Point(512, 101);
+            this.btnBorrar.Name = "btnBorrar";
+            this.btnBorrar.Size = new System.Drawing.Size(101, 39);
+            this.btnBorrar.TabIndex = 5;
+            this.btnBorrar.Text = "Borrar";
+            this.btnBorrar.UseVisualStyleBackColor = false;
+            this.btnBorrar.Click += new System.EventHandler(this.btnBorrar_Click);
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.BackColor = System.Drawing.Color.Red;
+            this.btnMostrar.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMostrar.Location = new System.Drawing.Point(392, 146);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(101, 39);
+            this.btnMostrar.TabIndex = 6;
+            this.btnMostrar.Text = "Listar";
+            this.btnMostrar.UseVisualStyleBackColor = false;
+            this.btnMostrar.Click += new System.EventHandler(this.btnMostrar_Click);
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.BackColor = System.Drawing.Color.SpringGreen;
+            this.btnActualizar.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnActualizar.Location = new System.Drawing.Point(512, 146);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(101, 39);
+            this.btnActualizar.TabIndex = 7;
+            this.btnActualizar.Text = "Actualizar";
+            this.btnActualizar.UseVisualStyleBackColor = false;
+            this.btnActualizar.Click += new System.EventHandler(this.btnActualizar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.BackColor = System.Drawing.Color.Red;
+            this.btnSalir.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalir.Location = new System.Drawing.Point(33, 101);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(101, 39);
+            this.btnSalir.TabIndex = 8;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = false;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // btnEliminarTodo
+            // 
+            this.btnEliminarTodo.BackColor = System.Drawing.Color.Red;
+            this.btnEliminarTodo.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEliminarTodo.Location = new System.Drawing.Point(439, 191);
+            this.btnEliminarTodo.Name = "btnEliminarTodo";
+            this.btnEliminarTodo.Size = new System.Drawing.Size(139, 39);
+            this.btnEliminarTodo.TabIndex = 9;
+            this.btnEliminarTodo.Text = "Eliminar todo";
+            this.btnEliminarTodo.UseVisualStyleBackColor = false;
+            this.btnEliminarTodo.Click += new System.EventHandler(this.btnEliminarTodo_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2});
+            this.dataGridView1.Location = new System.Drawing.Point(140, 101);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(246, 242);
+            this.dataGridView1.TabIndex = 11;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Nombre";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Calificación";
+            this.Column2.Name = "Column2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.ClientSize = new System.Drawing.Size(799, 472);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnEliminarTodo);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnActualizar);
+            this.Controls.Add(this.btnMostrar);
+            this.Controls.Add(this.btnBorrar);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.txtCalificacion);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblCalificacion);
+            this.Controls.Add(this.lblName);
+            this.Name = "Form1";
+            this.Text = "Calificaciones";
+            this.TransparencyKey = System.Drawing.Color.Green;
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblCalificacion;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtCalificacion;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnBorrar;
+        private System.Windows.Forms.Button btnMostrar;
+        private System.Windows.Forms.Button btnActualizar;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button btnEliminarTodo;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+    }
+}
+
---
+++ CalificacionesAlumnos/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalificacionesAlumnos
+{
+    public partial class Form1 : Form
+    {
+        public string _Nombre = string.Empty;
+        public double _Calificacion = 0;
+        public double[] _ListCalificaciones = new double[20];
+        public string[] _ListNombres = new string[20];
+        public int x = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            if (txtName.Text == string.Empty && txtCalificacion.Text == string.Empty)
+            {
+                MessageBox.Show("Debes ingresar el Nombre y la calificación", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                _Nombre = txtName.Text;
+                _ListNombres[x] = _Nombre;
+                _Calificacion = double.Parse(txtCalificacion.Text);
+                _ListCalificaciones[x] = _Calificacion;
+                x++;
+                MessageBox.Show("Se registro la informacion", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txtName.Text = "";
+                txtCalificacion.Text = "";
+            }
+           
+
+        }
+
+        private void btnBorrar_Click(object sender, EventArgs e)
+        {
+            int i;
+
+            if (txtName.Text != string.Empty)
+            {
+             
+                    _Nombre = (txtName.Text);
+                for (i = 0; i < 10; i++)
+                {
+                    while (_ListNombres[i] == _Nombre)
+                    {
+                        _ListCalificaciones[i] = _ListCalificaciones[i + 1];
+                        _ListNombres[i] = _ListNombres[i + 1];
+                        _ListCalificaciones[i + 1] = _ListCalificaciones[i + 2];
+                        _ListNombres[i + 1] = _ListNombres[i + 2];
+                        _ListCalificaciones[i + 2] = _ListCalificaciones[i + 3];
+                        _ListNombres[i + 2] = _ListNombres[i + 3];
+                        _ListCalificaciones[i + 3] = _ListCalificaciones[i + 4];
+                        _ListNombres[i + 3] = _ListNombres[i + 4];
+                        _ListCalificaciones[i + 4] = _ListCalificaciones[i + 5];
+                        _ListNombres[i + 4] = _ListNombres[i + 5];
+                        _ListCalificaciones[i + 5] = _ListCalificaciones[i + 6];
+                        _ListNombres[i + 5] = _ListNombres[i + 6];
+                        _ListCalificaciones[i + 6] = _ListCalificaciones[i + 7];
+                        _ListNombres[i + 6] = _ListNombres[i + 7];
+                        _ListCalificaciones[i + 7] = _ListCalificaciones[i + 8];
+                        _ListNombres[i + 8] = _ListNombres[i + 8];
+                        _ListCalificaciones[i + 8] = _ListCalificaciones[i + 9];
+                        _ListNombres[i + 9] = _ListNombres[i + 9];
+                        _ListCalificaciones[i + 9] = 0;
+                        _ListNombres[i + 10] = "";
+
+                    }
+                }
+                i = 1 - i;
+                MessageBox.Show("Se elimino el registro correctamente", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txtName.Text = "";
+                txtCalificacion.Text = "";
+                ListarDatos();
+
+            }
+            else
+            {
+                MessageBox.Show("Introduce el nombre del registro a borrar", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            ListarDatos();
+        }
+
+        public void ListarDatos()
+        {
+            int i;
+            dataGridView1.Rows.Clear();
+            for (i = 0; i < 10; i++)
+            {
+                dataGridView1.Rows.Add(_ListNombres[i], _ListCalificaciones[i].ToString());
+                if (i > 10)
+                {
+                    MessageBox.Show("No se puede ingresar más datos ", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            int i;
+            string nom;
+            if (txtName.Text != "")
+            {                nom = (txtName.Text);
+                for (i = 0; i < _ListCalificaciones.Length; i++)
+                {
+                    if (nom == _ListNombres[i])
+                    {
+                        _Calificacion = double.Parse(txtCalificacion.Text);
+                        _ListCalificaciones[i] = _Calificacion;
+                    }
+                }
+                txtName.Text = "";
+                txtCalificacion.Text = "";
+                ListarDatos();
+            }
+            else
+            {
+                MessageBox.Show("Introduce el nombre a actualizar", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnEliminarTodo_Click(object sender, EventArgs e)
+        {
+            for (var i = 0; i < _ListCalificaciones.Length; i++)
+            {
+                _ListCalificaciones[i] = 0;
+                _ListNombres[i] = string.Empty;
+            }
+            ListarDatos();
+        }
+    }
+    public class Calificaciones
+    {
+        public string Nombre { get; set; }
+        public double Calificacion { get; set; }
+
+    }
+}
